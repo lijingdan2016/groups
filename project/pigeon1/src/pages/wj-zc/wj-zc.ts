04b42a49ba8341af02b7179ff4798d8236dc5f51
housuhong2017 <371926913@qq.com>
2018-12-05 08:37:51 +0800

    1

--- project/pigeon1/src/pages/wj-zc/wj-zc.ts
+++ project/pigeon1/src/pages/wj-zc/wj-zc.ts
@@ -19,6 +19,19 @@ export class WjZcPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  logIn(username: HTMLInputElement, password: HTMLInputElement) {
+    if (username.value.length == 0) {
+      alert("请输入账号");
+    } else if (password.value.length == 0) {
+      alert("请输入密码");
+    } else {
+      let userinfo: string = '用户名：' + username.value + '密码：' + password.value;
+      // alert(userinfo);
+      console.log(userinfo);
+      this.navCtrl.push(HshDlPage,userinfo);
+    }
+  }
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad WjZcPage');
   }
