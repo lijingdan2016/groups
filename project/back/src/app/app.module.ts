5b179307814003c66efdea9ac5908bfb7f15796e
lijianhui1997 <1774407914@qq.com>
2018-12-07 09:09:40 +0800

    commit

--- project/back/src/app/app.module.ts
+++ project/back/src/app/app.module.ts
@@ -5,17 +5,19 @@ import {RouterModule,Router}  from '@angular/router';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
+import { SystemComponent } from './components/system/system.component';
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    SidebarComponent
+    SidebarComponent,
+    SystemComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path:'users',component:UsersComponent},
+      {path:'system',component:SystemComponent},
     ])
   ],
   providers: [],
