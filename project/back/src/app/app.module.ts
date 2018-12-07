16ca0780bd62b01d54bb9cb4f205160d128a496f
lijingdan2016 <973327127@qq.com>
2018-12-07 08:45:34 +0800

    提价

--- project/back/src/app/app.module.ts
+++ project/back/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule,Router}  from '@angular/router';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path:'users',component:UsersComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
