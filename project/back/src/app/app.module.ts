bca6c8136951845a4d4008fccf484e5ca461da9a
lijingdan2016 <973327127@qq.com>
2018-12-07 09:56:38 +0800

    提交

--- project/back/src/app/app.module.ts
+++ project/back/src/app/app.module.ts
@@ -5,32 +5,23 @@ import {RouterModule,Router}  from '@angular/router';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-<<<<<<< HEAD
 import { CardsComponent } from './components/cards/cards.component';
-
-=======
 import { SystemComponent } from './components/system/system.component';
->>>>>>> 5b179307814003c66efdea9ac5908bfb7f15796e
+
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     SidebarComponent,
-<<<<<<< HEAD
-    CardsComponent
-=======
+    CardsComponent,
     SystemComponent
->>>>>>> 5b179307814003c66efdea9ac5908bfb7f15796e
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path:'users',component:UsersComponent},
-<<<<<<< HEAD
       {path:'cards',component:CardsComponent},
-=======
       {path:'system',component:SystemComponent},
->>>>>>> 5b179307814003c66efdea9ac5908bfb7f15796e
     ])
   ],
   providers: [],
