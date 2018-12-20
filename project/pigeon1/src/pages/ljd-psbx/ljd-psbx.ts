2194b08da08d479fb175550c9e84ca99cb62a8bd
wangjie199737 <419009594@>
2018-12-20 18:51:16 +0800

    王洁更新

--- project/pigeon1/src/pages/ljd-psbx/ljd-psbx.ts
+++ project/pigeon1/src/pages/ljd-psbx/ljd-psbx.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 /**
  * Generated class for the LjdPsbxPage page.
  *
@@ -14,12 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'ljd-psbx.html',
 })
 export class LjdPsbxPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private headers = new HttpHeaders({'Content-Type':'application/json'});
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient,public viewCtrl:ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LjdPsbxPage');
   }
-
+  look(student:HTMLInputElement){
+    let a={stu:student.value};
+    this.http.post('/parent_behave/registe',a,{
+      headers : this.headers,
+      observe : 'body',
+      responseType : 'json'
+    }
+    ).subscribe(data=>{
+      console.log(data);
+    })
+  }
 }
--- project/pigeon1/src/pages/syr-notice/syr-notice.ts
+++ project/pigeon1/src/pages/syr-notice/syr-notice.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 /**
  * Generated class for the SyrNoticePage page.
  *
@@ -14,12 +14,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'syr-notice.html',
 })
 export class SyrNoticePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private headers = new HttpHeaders({'Content-Type':'application/json'});
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http:HttpClient,public viewCtrl:ViewController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SyrNoticePage');
+  }
+  save(student:HTMLInputElement,content: HTMLInputElement){
+    let sign: string = student.value+'的平时表现为' + content.value ;
+    console.log(sign);
+    // this.navCtrl.push(sign);
+    let a={stu:student.value,text:content.value};
+    this.http.post('/teacher_behave/registe',a,{
+      headers : this.headers,
+      observe : 'body',
+      responseType : 'json'
+    }
+    ).subscribe(data=>{
+      console.log(data);
+    })
   }
 
 }
