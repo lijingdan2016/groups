5875fe43b7ae801d805f7f32ae01db7da7ebe5aa
lijianhui1997 <1774407914@qq.com>
2018-12-27 19:45:43 +0800

    后台帖子管理提交

--- project/pigeonback/src/app/bz-modules/inventory/inbound-receipt-table/inbound-receipt-table.component.ts
+++ project/pigeonback/src/app/bz-modules/inventory/inbound-receipt-table/inbound-receipt-table.component.ts
@@ -4,7 +4,9 @@ import { SelectItem } from "primeng/primeng";
 import { WarehouseService } from "../../../common/services/warehouse.service";
 import { CategoryService } from "../../../common/services/category.service";
 import { InboundReceiptService } from "../../../common/services/inbound-receipt.service";
-
+import { HttpClient } from '@angular/common/http';
+import { ConfirmationService } from "primeng/components/common/confirmationservice";
+import { MessageService } from "primeng/components/common/messageservice";
 @Component({
   selector: "inbound-receipt-table",
   templateUrl: "./inbound-receipt-table.component.html",
@@ -17,19 +19,22 @@ export class InboundReceiptTableComponent implements OnInit {
   public endDate: Date;
   public items: Array<any>;
   public selectedInbentory;
-
+  a;
   constructor(
+    public http:HttpClient,
     private router: Router,
+    private confirmationService: ConfirmationService,
     private activeRoute: ActivatedRoute,
     private warehouseService: WarehouseService,
     private categoryService: CategoryService,
+    private messageService: MessageService,
     private inboundReceiptService: InboundReceiptService
   ) {}
 
   ngOnInit() {
     this.startDate = new Date();
     this.endDate = new Date();
-
+    
     this.warehouseService.getWarehouses().subscribe(warehouses => {
       this.warehouses = warehouses;
     });
@@ -40,6 +45,10 @@ export class InboundReceiptTableComponent implements OnInit {
     this.inboundReceiptService.getInboundRecords().subscribe(items => {
       this.items = items;
     });
+    this.http.get('/backuser/tiezi').subscribe(data=>{
+      this.a=data;
+      console.log(data);
+    })
   }
 
   public newReceipt() {
@@ -53,4 +62,24 @@ export class InboundReceiptTableComponent implements OnInit {
   public printReceipt() {
     window.print();
   }
+  
+  del(i:number,item:Object){
+    let a=item;
+    this.a.splice(i,1);
+    
+    this.http.post('/backuser/del',{a}).subscribe(data=>{
+      console.log(data);
+    })
+  }
+  public delItem(item) {
+    console.log(item);
+    this.confirmationService.confirm({
+      message: '确定要删除吗？',
+      accept: () => {
+        console.log(item);
+        this.messageService.add({ severity: 'success', summary: '成功', detail: '删除数据成功' });
+      }
+    });
+  }
+
 }
