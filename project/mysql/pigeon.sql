6516fce99b8ba597228eea9bfa93f11e24123974
songyingran <859324331@qq.com>
2018-12-27 18:37:18 +0800

    数据库

--- project/mysql/pigeon.sql
+++ project/mysql/pigeon.sql
@@ -11,6 +11,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
+
+-- 导出 pigeon 的数据库结构
+CREATE DATABASE IF NOT EXISTS `pigeon` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `pigeon`;
+
 -- 导出  表 pigeon.admin 结构
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
@@ -168,15 +173,15 @@ DELETE FROM `task`;
 -- 导出  表 pigeon.tiezi 结构
 DROP TABLE IF EXISTS `tiezi`;
 CREATE TABLE IF NOT EXISTS `tiezi` (
-  `tiezi_id` int(11) NOT NULL,
+  `tiezi_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `tiezi_content` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `tiezi_date` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `zan` int(11) DEFAULT NULL,
+  `zan` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`tiezi_id`),
   KEY `FK_tz` (`user_id`),
   CONSTRAINT `FK_tz` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='帖子表';
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='帖子表';
 
 -- 正在导出表  pigeon.tiezi 的数据：~0 rows (大约)
 DELETE FROM `tiezi`;
