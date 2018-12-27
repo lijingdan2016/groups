e613e6d4cb6911df476e67f2e4854e12fc076414
songyingran <859324331@qq.com>
2018-12-27 19:24:39 +0800

    提交

--- project/backend/routes/shequ.js
+++ project/backend/routes/shequ.js
@@ -4,12 +4,12 @@ const db = require('../model/database.js');
 
 
 router.post('/sq',function(req,res){
-  var user_id = req.body.uid;
+ // var user_id = req.body.userid;
   res.header('Access-Control-Allow-Origin','*');
   res.header('Content-Type','text/plain; charset = "utf-8"');
-  const sql = 'select * from tiezi where user_id = ' + user_id;
+  const sql = 'select * from tiezi where user_id = 100';
   //if(user_id){
-    db.query(sql,[user_id],(err,result) => {
+    db.query(sql,(err,result) => {
       if(err){
         console.error("Error:",err);
         process.exit();
@@ -21,6 +21,25 @@ router.post('/sq',function(req,res){
     console.log(req.body);
   //}
 })
+
+router.post('/es',function(req,res){
+  var tiezi_id = req.body.tzid;
+
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Content-Type','text/plain; charset = "utf-8"');
+  const sql = 'select * from tiezi where tiezi_id = ' + tiezi_id;
+  db.query(sql,[tiezi_id],(err,result) => {
+    if(err){
+      console.error("Error:",err);
+      process.exit();
+    }
+    res.send(JSON.stringify({
+      data:result
+    }));
+  });
+})
+
+
 
 router.post('/sqtuijian',function(req,res){
 
